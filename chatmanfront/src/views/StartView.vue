75e96acc4880ad4c1463de68931b287b6106bbf3
Morales-10 <anna450s@easv365.dk>
2022-05-03 13:40:59 +0200

    vue

--- chatmanfront/src/views/StartView.vue
+++ chatmanfront/src/views/StartView.vue
@@ -7,7 +7,9 @@
       <div class="center_man">
         <InputText v-model="inputUsername" style="margin-bottom: 10px" placeholder="Enter Username" />
         <InputText type="password" v-model="inputPassword" style="margin-bottom: 10px" placeholder="Enter password" /> <br />
+        <h8> Register Account </h8>
         <Button @click="handleLoginUser" class="p-button-rounded p-button-secondary " > Sign in </Button>
+
       </div>
     </div>
 
