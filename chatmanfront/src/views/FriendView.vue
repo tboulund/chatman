d7d1a0a1f02c7fa83c3f3c4776e22618df4b205e
WackyGlow <stampe12@gmail.com>
2022-05-03 14:21:58 +0200

    Update FriendView.vue

--- chatmanfront/src/views/FriendView.vue
+++ chatmanfront/src/views/FriendView.vue
@@ -1,9 +1,23 @@
-<template>
-
+-<template #option="slotProps">
+  <Listbox v-model="selectedFriends" :options="name" :multiple="true" :filter="true" optionLabel="Username" listStyle="max-height:250px" style="width:15rem" filterPlaceholder="Search">
+      <div class="friend-item">
+        <div>{{slotProps.option.name}}</div>
+      </div>
+  </Listbox>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
+  setup() {
+    const selectedFriends = ref();([
+      {name: 'Anna'},
+      {name: 'Rasmus'},
+    ]);
+    const items = ref(Array.from({ length: 100000 }, (_, i) => ({ label: `Item #${i}`, value: i })));
+
+    return {selectedFriends,name,}
+  },
   name: "FriendView.vue"
 };
 </script>
