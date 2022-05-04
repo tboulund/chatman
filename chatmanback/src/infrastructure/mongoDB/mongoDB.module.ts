0706ba068384bed1614cbca80b51b1db532df8f0
pete9991 <peter.lydeking@gmail.com>
2022-05-04 08:54:32 +0200

    attempted to fix file path for jenkins

--- chatmanback/src/infrastructure/mongoDB/mongoDB.module.ts
+++ chatmanback/src/infrastructure/mongoDB/mongoDB.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongoDbProvider } from './mongoDb.provider';
+import { MongoDbProvider } from '../mongoDB/mongoDB.provider';
 
 @Module({ providers: [MongoDbProvider], exports: [MongoDbProvider] })
 export class MongoDbModule {}
